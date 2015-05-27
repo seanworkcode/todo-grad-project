89d513b84ea89212b9a67285a83cd4c3f1cb7751
Joe Bandenburg <joe@bandenburg.com>
2015-05-27 17:28:04 +0100

    Implement CRUD RESTful API for TODOs

--- server.js
+++ server.js
@@ -1,10 +1,72 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.get('/', function(req, res) {
-    res.send('Hello CD World!');
+app.use(express.static("public"));
+app.use(bodyParser.json());
+
+var latestId = 0;
+var todos = [];
+
+// Create
+app.put("/api/todo", function(req, res) {
+    var todo = req.body;
+    todo.id = latestId.toString();
+    latestId++;
+    todos.push(todo);
+    res.sendStatus(200);
+});
+
+// Read
+app.get("/api/todo", function(req, res) {
+    res.json(todos);
+});
+
+app.get("/api/todo/:id", function(req, res) {
+    var id = req.params.id;
+    var todo = getTodo(id);
+    if (todo) {
+        res.json(todo);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+// Update
+app.post("/api/todo/:id", function(req, res) {
+    var id = req.params.id;
+    var todo = getTodo(id);
+    var newTodo = req.body;
+    newTodo.id = id;
+    if (todo) {
+        var index = todos.indexOf(todo);
+        todos[index] = newTodo;
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+// Delete
+app.delete("/api/todo/:id", function(req, res) {
+    var id = req.params.id;
+    var todo = getTodo(id);
+    if (todo) {
+        todos = todos.filter(function(otherTodo) {
+            return otherTodo !== todo;
+        });
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 app.listen(port);
+
+function getTodo(id) {
+    return todos.filter(function(todo) {
+        return todo.id === id;
+    })[0];
+}
