e41a6598f9b690a1c2a28ccc1edc4b34a5f90b9e
Joe Bandenburg <joe@bandenburg.com>
2015-05-30 17:00:30 +0100

    Output test results as XML file for CI

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,24 @@
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks("grunt-mocha-test");
 
+    var testOutputLocation = process.env.CIRCLE_TEST_REPORTS || "test_output";
     grunt.initConfig({
         mochaTest: {
-	    test: {
-	        src: ['test/**/*.js']
-	    }
-	}
+            test: {
+                src: ["test/**/*.js"]
+            },
+            ci: {
+                src: ["test/**/*.js"],
+                options: {
+                    reporter: "xunit",
+                    captureFile: testOutputLocation + "/mocha/results.xml",
+                    quiet: true
+                }
+            }
+        }
     });
 
-    grunt.registerTask('test', 'mochaTest');
-    grunt.registerTask('default', 'test');
+    grunt.registerTask("test", "mochaTest:test");
+    grunt.registerTask("ci-test", "mochaTest:ci");
+    grunt.registerTask("default", "test");
 };
