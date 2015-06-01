aa1a1cdc3c36eb21fdfc93af68ff1ed453424033
David Thomas <dthomas@scottlogic.co.uk>
2015-06-01 15:13:53 +0100

    Refactored end 2 end tests to a more BDD style.

--- test/e2e.js
+++ test/e2e.js
@@ -1,112 +1,71 @@
-var express = require("express");
-var server = require("../server/server");
-var webdriver = require("selenium-webdriver");
 var testing = require("selenium-webdriver/testing");
 var assert = require("chai").assert;
-var istanbul = require("istanbul");
-var path = require("path");
-var fs = require("fs");
-
-var testPort = 52684;
-var baseUrl = "http://localhost:" + testPort;
-var instrumenter = new istanbul.Instrumenter();
-var collector = new istanbul.Collector();
-var gatheringCoverage = process.env.running_under_istanbul;
-var coverageFilename = "build_artifacts/coverage-e2e.json";
+var helpers = require("./e2eHelpers");
 
 testing.describe("end to end", function() {
-    var router;
-    var serverInstance;
-    var driver;
+    var server;
+
+    this.timeout(10000);
+
     testing.beforeEach(function() {
-        router = express.Router();
-        if (gatheringCoverage) {
-            router.get("/main.js", function(req, res) {
-                var absPath = path.join(__dirname, "..", "public", "main.js");
-                res.send(instrumenter.instrumentSync(fs.readFileSync("public/main.js", "utf8"), absPath));
-            });
-        }
-        serverInstance = server(testPort, router);
-        driver = new webdriver.Builder().forBrowser("chrome").build();
+        server = helpers.setupServer();
     });
     testing.afterEach(function() {
-        if (gatheringCoverage) {
-            driver.executeScript("return __coverage__;").then(function (coverage) {
-                collector.add(coverage);
-            });
-        }
-        serverInstance.close();
-        driver.quit();
+        helpers.teardownServer(server);
     });
-    if (gatheringCoverage) {
-        testing.after(function () {
-            fs.writeFileSync(coverageFilename, JSON.stringify(collector.getFinalCoverage()), "utf8");
-        });
-    }
+    testing.after(function() {
+        helpers.reportCoverage();
+    });
+
     testing.describe("on page load", function() {
         testing.it("displays TODO title", function() {
-            driver.get(baseUrl);
-            driver.findElement(webdriver.By.css("h1")).getText().then(function(text) {
+            helpers.navigateToSite(server);
+            helpers.getTitleText(server).then(function(text) {
                 assert.equal(text, "TODO List");
             });
         });
         testing.it("displays empty TODO list", function() {
-            driver.get(baseUrl);
-            driver.findElements(webdriver.By.css("#todo-list li")).then(function(elements) {
+            helpers.navigateToSite(server);
+            helpers.getTodoList(server).then(function(elements) {
                 assert.equal(elements.length, 0);
             });
         });
         testing.it("displays an error if the request fails", function() {
-            router.get("/api/todo", function(req, res) {
-                res.sendStatus(500);
-            });
-            driver.get(baseUrl);
-            var errorElement = driver.findElement(webdriver.By.id("error"));
-            driver.wait(webdriver.until.elementTextContains(errorElement, "Failed"), 500);
-            errorElement.getText().then(function(text) {
+            helpers.setupErrorRoute(server, "get", "/api/todo");
+            helpers.navigateToSite(server);
+            helpers.getErrorText(server).then(function(text) {
                 assert.equal(text, "Failed to get list. Server returned 500 - Internal Server Error");
             });
         });
     });
     testing.describe("on create todo item", function() {
         testing.it("clears the input field", function() {
-            driver.get(baseUrl);
-            driver.findElement(webdriver.By.id("new-todo")).sendKeys("New todo item");
-            driver.findElement(webdriver.By.id("submit-todo")).click();
-            driver.findElement(webdriver.By.id("new-todo")).getAttribute("value").then(function(value) {
+            helpers.navigateToSite(server);
+            helpers.addTodo(server, "New todo item");
+            helpers.getInputText(server).then(function(value) {
                 assert.equal(value, "");
             });
         });
         testing.it("adds the todo item to the list", function() {
-            driver.get(baseUrl);
-            driver.findElement(webdriver.By.id("new-todo")).sendKeys("New todo item");
-            driver.findElement(webdriver.By.id("submit-todo")).click();
-            driver.wait(webdriver.until.elementsLocated(webdriver.By.css("#todo-list li")), 500);
-            driver.findElements(webdriver.By.css("#todo-list li")).then(function(elements) {
+            helpers.navigateToSite(server);
+            helpers.addTodo(server, "New todo item");
+            helpers.getTodoList(server).then(function(elements) {
                 assert.equal(elements.length, 1);
             });
         });
         testing.it("displays an error if the request fails", function() {
-            router.put("/api/todo", function(req, res) {
-                res.sendStatus(500);
-            });
-            driver.get(baseUrl);
-            driver.findElement(webdriver.By.id("new-todo")).sendKeys("New todo item");
-            driver.findElement(webdriver.By.id("submit-todo")).click();
-            var errorElement = driver.findElement(webdriver.By.id("error"));
-            driver.wait(webdriver.until.elementTextContains(errorElement, "Failed"), 500);
-            errorElement.getText().then(function(text) {
+            helpers.setupErrorRoute(server, "put", "/api/todo");
+            helpers.navigateToSite(server);
+            helpers.addTodo(server, "New todo item");
+            helpers.getErrorText(server).then(function(text) {
                 assert.equal(text, "Failed to create item. Server returned 500 - Internal Server Error");
             });
         });
         testing.it("can be done multiple times", function() {
-            driver.get(baseUrl);
-            driver.findElement(webdriver.By.id("new-todo")).sendKeys("New todo item");
-            driver.findElement(webdriver.By.id("submit-todo")).click();
-            driver.findElement(webdriver.By.id("new-todo")).sendKeys("Another new todo item");
-            driver.findElement(webdriver.By.id("submit-todo")).click();
-            driver.wait(webdriver.until.elementsLocated(webdriver.By.css("#todo-list li")));
-            driver.findElements(webdriver.By.css("#todo-list li")).then(function(elements) {
+            helpers.navigateToSite(server);
+            helpers.addTodo(server, "New todo item");
+            helpers.addTodo(server, "Another new todo item");
+            helpers.getTodoList(server).then(function(elements) {
                 assert.equal(elements.length, 2);
             });
         });
---
+++ test/e2eHelpers.js
@@ -0,0 +1,82 @@
+var express = require("express");
+var server = require("../server/server");
+var webdriver = require("selenium-webdriver");
+var istanbul = require("istanbul");
+var path = require("path");
+var fs = require("fs");
+
+var testPort = 52684;
+var baseUrl = "http://localhost:" + testPort;
+var instrumenter = new istanbul.Instrumenter();
+var collector = new istanbul.Collector();
+var gatheringCoverage = process.env.running_under_istanbul;
+var coverageFilename = "build_artifacts/coverage-e2e.json";
+
+module.exports.setupServer = function() {
+    var router = express.Router();
+    if (gatheringCoverage) {
+        router.get("/main.js", function(req, res) {
+            var absPath = path.join(__dirname, "..", "public", "main.js");
+            res.send(instrumenter.instrumentSync(fs.readFileSync("public/main.js", "utf8"), absPath));
+        });
+    }
+    var serverInstance = server(testPort, router);
+    var driver = new webdriver.Builder().forBrowser("chrome").build();
+    return {router: router, server: serverInstance, driver: driver};
+};
+
+module.exports.teardownServer = function(server) {
+    if (gatheringCoverage) {
+        server.driver.executeScript("return __coverage__;").then(function (coverage) {
+            collector.add(coverage);
+        });
+    }
+    server.server.close();
+    server.driver.quit();
+};
+
+module.exports.reportCoverage = function() {
+    if (gatheringCoverage) {
+        fs.writeFileSync(coverageFilename, JSON.stringify(collector.getFinalCoverage()), "utf8");
+    }
+};
+
+module.exports.navigateToSite = function(server) {
+    server.driver.get(baseUrl);
+};
+
+module.exports.getTitleText = function(server) {
+    return server.driver.findElement(webdriver.By.css("h1")).getText();
+};
+
+module.exports.getInputText = function(server) {
+    return server.driver.findElement(webdriver.By.id("new-todo")).getAttribute("value");
+};
+
+module.exports.getErrorText = function(server) {
+    var errorElement = server.driver.findElement(webdriver.By.id("error"));
+    server.driver.wait(webdriver.until.elementTextContains(errorElement, "Failed"), 500);
+    return errorElement.getText();
+};
+
+module.exports.getTodoList = function(server) {
+    return server.driver.findElements(webdriver.By.css("#todo-list li"));
+};
+
+module.exports.addTodo = function(server, text) {
+    server.driver.findElement(webdriver.By.id("new-todo")).sendKeys(text);
+    server.driver.findElement(webdriver.By.id("submit-todo")).click();
+};
+
+module.exports.setupErrorRoute = function(server, action, route) {
+    if (action === "get") {
+        server.router.get(route, function(req, res) {
+            res.sendStatus(500);
+        });
+    }
+    if (action === "put") {
+        server.router.put(route, function(req, res) {
+            res.sendStatus(500);
+        });
+    }
+};
