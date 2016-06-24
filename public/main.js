03be45b93dc20c5100eb698385a3212571a0616b
unknown <joconnor@WS00034.scottlogic.co.uk>
2016-06-24 14:25:11 +0100

    Added completeTodo function, as well as a complete button. Tweaked some of the style options for the completebutton and the listItem fonts in order to make the website as ugly as possible

--- public/main.js
+++ public/main.js
@@ -52,13 +52,24 @@ function reloadTodoList() {
         todos.forEach(function(todo) {
             var listItem = document.createElement("li");
             var deleteBtn = document.createElement("button");
+            var completeBtn = document.createElement("button");
             listItem.textContent = todo.title;
             deleteBtn.textContent = "Delete";
+            completeBtn.textContent = "Complete";
             deleteBtn.style.background = "black";
             deleteBtn.style.color = "red";
+            completeBtn.style.background = "red";
+            if (todo.isComplete) {
+                listItem.style.color = "green";
+                listItem.style.fontFamily = "Impact";
+            }
             deleteBtn.addEventListener("click", function() {
                 deleteTodo(todo);
             });
+            completeBtn.addEventListener("click", function() {
+                completeTodo(todo);
+            });
+            listItem.appendChild(completeBtn);
             listItem.appendChild(deleteBtn);
             todoList.appendChild(listItem);
         });
@@ -74,6 +85,18 @@ function deleteTodo(todo) {
             reloadTodoList();
         } else {
             error.textContent = "Failed to delete. Server returned " + this.status + " - " + this.responseText;
+        }
+    };
+    createRequest.send();
+}
+
+function completeTodo(todo) {
+    var createRequest = new XMLHttpRequest();
+    createRequest.open("PUT", "/api/todo/" + todo.id);
+    createRequest.setRequestHeader("Content-type", "application/json");
+    createRequest.onload = function() {
+        if (this.status === 200) {
+            reloadTodoList();
         }
     };
     createRequest.send();
