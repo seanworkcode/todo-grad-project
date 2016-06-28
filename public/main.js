593149ac6de06063115bf5a5f848ef19ff003cf2
Jamie Zafar <jo13358@my.bristol.ac.uk>
2016-06-28 10:59:20 +0100

    Added filtering buttons (ALL, ACTIVE, COMPLETED)

--- public/main.js
+++ public/main.js
@@ -4,6 +4,10 @@ var form = document.getElementById("todo-form");
 var todoTitle = document.getElementById("new-todo");
 var error = document.getElementById("error");
 var countLabel = document.getElementById("count-label");
+var FILTER_ALL = 0;
+var FILTER_ACTIVE = 1;
+var FILTER_COMPLETED = 2;
+var filter = FILTER_ALL;
 
 form.onsubmit = function(event) {
     var title = todoTitle.value;
@@ -61,7 +65,7 @@ function updateIncompletes(todos, countLabel) {
     var incompleteCount = todos.reduce(function(p, c) {
         return (!c.isComplete ? p + 1 : p);
     }, 0);
-    countLabel.textContent = incompleteCount.toString();
+    countLabel.textContent = incompleteCount.toString() + " incomplete Todos";
     if (incompleteCount !== todos.length) {
         countLabel.appendChild(makeButton("Delete Completed", deleteCompletedTodos));
     }
@@ -74,9 +78,27 @@ function reloadTodoList() {
     }
     todoListPlaceholder.style.display = "block";
     getTodoList(function(todos) {
+        var filteredTodos;
         todoListPlaceholder.style.display = "none";
         updateIncompletes(todos, countLabel);
-        todos.forEach(function(todo) {
+        switch (filter) {
+            case FILTER_ALL:
+                filteredTodos = todos;
+                break;
+            case FILTER_ACTIVE:
+                filteredTodos = todos.filter(function(todo) {
+                    return !todo.isComplete;
+                });
+                break;
+            case FILTER_COMPLETED:
+                filteredTodos = todos.filter(function(todo) {
+                    return todo.isComplete;
+                });
+                break;
+            default:
+                filteredTodos = todos;
+        }
+        filteredTodos.forEach(function(todo) {
             var listItem = document.createElement("li");
             var deleteBtn = makeButton("Delete", deleteTodo.bind(null, todo));
             var completeBtn = makeButton("Complete", completeTodo.bind(null, todo));
@@ -136,6 +158,11 @@ function completeTodo(todo) {
         }
     };
     createRequest.send(JSON.stringify({isComplete: !todo.isComplete}));
+}
+
+function setFilter(filterType) {
+    filter = filterType;
+    reloadTodoList();
 }
 
 reloadTodoList();
